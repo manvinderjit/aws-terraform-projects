030a58eaa9698d7e111f13d9af964d7f950cb50a
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-08-06 20:51:42 -0400

    cicd: update ci cd to apply

--- s3-website/main.tf
+++ s3-website/main.tf
@@ -1,7 +1,7 @@
-resource "aws_s3_bucket" "example" {    
+resource "aws_s3_bucket" "example" {
 
-    tags = {
-        ManagedBy        = "GitAwsTerraformProjects"
-        Project          = "GitAwsTerraformProjects"
-    }
+  tags = {
+    ManagedBy = "GitAwsTerraformProjects"
+    Project   = "GitAwsTerraformProjects"
+  }
 }
