83f67d27a05e19548d86cf4316ebd16d4c0eb415
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-08-28 22:09:56 -0400

    fix: add datablock to get ami id

--- subnet-ec2-rds/main.tf
+++ subnet-ec2-rds/main.tf
@@ -110,6 +110,16 @@ resource "aws_security_group" "rds_sg" {
   }
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
 resource "aws_instance" "web" {
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
