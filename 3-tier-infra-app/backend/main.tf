9d4fd2eab17b2567349778a76ac3a94388474508
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-09-02 00:43:26 -0400

    infra: add backend infra

---
+++ 3-tier-infra-app/backend/main.tf
@@ -0,0 +1,80 @@
+provider "aws" {
+    region = var.aws_region
+    # Provide default tags to identify all resources
+    default_tags {
+      tags = {      
+        ManagedBy = "GitAwsTerraformProjects"
+        Project   = "three-tier-infra-app"
+      }
+    }
+}
+
+data "terraform_remote_state" "base_infra" {
+  backend = "s3"   # or whatever backend you use (S3/DynamoDB, etc)
+
+  config = {
+    bucket         = var.tfstate_bucket
+    key            = var.base_tfstate_key  # exact key for base infra tfstate
+    region         = var.aws_region
+    dynamodb_table = var.tfstate_lock_table
+  }
+}
+
+
+resource "aws_launch_template" "backend_lt" {
+  name_prefix   = "three-tier-backend-lt"
+  image_id      = var.ec2_ami_id_backend
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [
+    data.terraform_remote_state.base_infra.outputs.private_ec2_security_group_id
+  ]
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              cd /home/ec2-user/app/target
+              export DB_URL=jdbc:mysql://${data.terraform_remote_state.base_infra.outputs.rds_endpoint}/${var.db_name}?createDatabaseIfNotExist=true
+              export DB_USERNAME=${var.db_username}
+              export DB_PASSWORD=${var.db_password}
+              nohup java -jar backend-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+              EOF
+            )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "three-tier-backend-ec2"
+    }
+  }
+}
+
+resource "aws_autoscaling_group" "backend_asg" {
+  name             = "three-tier-backend-asg"
+  desired_capacity = 2
+  max_size         = 3
+  min_size         = 1
+
+  vpc_zone_identifier = data.terraform_remote_state.base_infra.outputs.private_subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.backend_lt.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [
+    data.terraform_remote_state.base_infra.outputs.api_target_group_arn
+  ]
+
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+
+  tag {
+    key                 = "Name"
+    value               = "three-tier-backend-ec2"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
--- 3-tier-infra-app/base-infra-rds/outputs.tf
+++ 3-tier-infra-app/base-infra-rds/outputs.tf
@@ -1,3 +1,18 @@
 output "rds_endpoint" {
   value = aws_db_instance.default.endpoint
 }
+
+output "private_subnet_ids" {
+  value = [
+    aws_subnet.three_tier_infra_app_subnet_private_1.id,
+    aws_subnet.three_tier_infra_app_subnet_private_2.id,
+  ]
+}
+
+output "private_ec2_security_group_id" {
+  value = aws_security_group.three_tier_infra_app_private_ec2_sg.id
+}
+
+output "api_target_group_arn" {
+  value = aws_lb_target_group.api_tg.arn
+}
