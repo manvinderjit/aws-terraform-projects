3fc47807d6d8a37c90315804634633089d5df514
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-09-03 16:45:56 -0400

    infra: add rds, alb

--- three-tier-infra-app-docker-ecr/main.tf
+++ three-tier-infra-app-docker-ecr/main.tf
@@ -269,3 +269,96 @@ resource "aws_db_subnet_group" "db_subnet_group" {
   name       = "three-tier-infra-app-ecr-rds-sbnt-grp"
   subnet_ids = [aws_subnet.three_tier_infra_app_ecr_subnet_rds_1.id, aws_subnet.three_tier_infra_app_ecr_subnet_rds_2.id]
 }
+
+# RDS Database
+resource "aws_db_instance" "three_tier_infra_app_ecr_dbs" {
+  identifier              = "three-tier-infra-ecr-db"
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  engine                  = "mysql"
+  engine_version          = "8.0.42"
+  instance_class          = "db.t4g.micro"    
+  username                = var.db_username
+  password                = var.db_password
+  db_name                 = var.db_name
+  db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  skip_final_snapshot     = true
+  publicly_accessible     = false
+  multi_az                = false
+  backup_retention_period = 0
+}
+
+# ALBs
+resource "aws_lb" "three_tier_infra_app_ecr_ws_alb" {
+  name               = "three-tier-infra-app-ws-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups   = [aws_security_group.three_tier_infra_app_ecr_fe_alb_sg.id]
+  subnets            = [
+    aws_subnet.three_tier_infra_app_ecr_subnet_public_1.id,
+    aws_subnet.three_tier_infra_app_ecr_subnet_public_2.id    
+  ]
+}
+
+resource "aws_lb_target_group" "three_tier_infra_app_ecr_ws_tg" {
+  name     = "three-tier-infra-app-ws-tg"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.three_tier_infra_app_ecr_vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "3000"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.three_tier_infra_app_ecr_ws_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.three_tier_infra_app_ecr_ws_tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "three_tier_infra_app_ecr_app_tg" {
+  name     = "three-tier-infra-app-ecr-app-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.three_tier_infra_app_ecr_vpc.id
+
+  health_check {
+    path                = "/api/movies"
+    protocol            = "HTTP"
+    port                = "8080"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener_rule" "api_route" {
+  listener_arn = aws_lb_listener.app_listener.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.three_tier_infra_app_ecr_app_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/api/*"]
+    }
+  }
+}
--- three-tier-infra-app-docker-ecr/variables.tf
+++ three-tier-infra-app-docker-ecr/variables.tf
@@ -15,3 +15,26 @@ variable "instance_type" {
   type        = string
   default     = "t3.nano"
 }
+
+variable "ec2_ami_id" {
+  description = "ami to be used for launching ec2"
+  type        = string
+}
+
+variable "db_username" {
+  description = "RDS database username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "RDS database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "RDS default database"
+  type        = string
+  sensitive   = true
+}
