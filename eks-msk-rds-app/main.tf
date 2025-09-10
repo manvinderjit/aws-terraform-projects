040ca6d1807982fc4177a107fe610a5e1a0b7279
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-09-10 19:23:19 -0400

    infra: add base infra

---
+++ eks-msk-rds-app/main.tf
@@ -0,0 +1,216 @@
+provider "aws" {
+    region = var.aws_region
+    # Provide default tags to identify all resources
+    default_tags {
+      tags = {      
+        ManagedBy = "GitAwsTerraformProjects"
+        Project   = "eks-msk-rds-app"
+      }
+    }
+}
+
+resource "aws_vpc" "eks_msk_rds_app_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "eks-msk-rds-app-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "eks_msk_rds_app_igw" {
+  vpc_id = aws_vpc.eks_msk_rds_app_vpc.id
+  tags = {
+    Name = "eks-msk-rds-app-igw"
+  }
+}
+
+resource "aws_route_table" "eks_msk_rds_app_public_rt" {
+  vpc_id = aws_vpc.eks_msk_rds_app_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_msk_rds_app_igw.id
+  }
+  tags = {
+    Name = "eks-msk-rds-app-route-table-public"
+  }
+}
+
+resource "aws_route_table" "eks_msk_rds_app_private_rt" {
+  vpc_id = aws_vpc.eks_msk_rds_app_vpc.id
+
+  tags = {
+    Name = "eks-msk-rds-app-route-table-private"
+  }
+}
+
+resource "aws_route_table" "eks_msk_rds_app_msk_rt" {
+  vpc_id = aws_vpc.eks_msk_rds_app_vpc.id
+  tags = {
+    Name = "eks-msk-rds-app-route-table-msk"
+  }
+}
+
+resource "aws_route_table" "eks_msk_rds_app_rds_rt" {
+  vpc_id = aws_vpc.eks_msk_rds_app_vpc.id
+
+  tags = {
+    Name = "eks-msk-rds-app-route-table-rds"
+  }
+}
+
+resource "aws_subnet" "eks_msk_rds_app_subnet_public_1" {
+  vpc_id                  = aws_vpc.eks_msk_rds_app_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "eks-msk-rds-app-subnet-public-1"
+  }
+}
+
+resource "aws_subnet" "eks_msk_rds_app_subnet_public_2" {
+  vpc_id                  = aws_vpc.eks_msk_rds_app_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "eks-msk-rds-app-subnet-public-2"
+  }
+}
+
+resource "aws_subnet" "eks_msk_rds_app_subnet_private_eks_1" {
+  vpc_id                  = aws_vpc.eks_msk_rds_app_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "eks-msk-rds-app-subnet-private-eks-1"
+  }
+}
+
+resource "aws_subnet" "eks_msk_rds_app_subnet_private_eks_2" {
+  vpc_id                  = aws_vpc.eks_msk_rds_app_vpc.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "eks-msk-rds-app-subnet-private-eks-2"
+  }
+}
+
+resource "aws_subnet" "eks_msk_rds_app_subnet_private_msk_1" {
+  vpc_id                  = aws_vpc.eks_msk_rds_app_vpc.id
+  cidr_block              = "10.0.5.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "eks-msk-rds-app-subnet-private-msk-1"
+  }
+}
+
+resource "aws_subnet" "eks_msk_rds_app_subnet_private_msk_2" {
+  vpc_id                  = aws_vpc.eks_msk_rds_app_vpc.id
+  cidr_block              = "10.0.6.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "eks-msk-rds-app-subnet-private-msk-2"
+  }
+}
+
+resource "aws_subnet" "eks_msk_rds_app_subnet_rds_1" {
+  vpc_id                  = aws_vpc.eks_msk_rds_app_vpc.id
+  cidr_block              = "10.0.7.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "eks-msk-rds-app-subnet-rds-1"
+  }
+}
+
+resource "aws_subnet" "eks_msk_rds_app_subnet_rds_2" {
+  vpc_id                  = aws_vpc.eks_msk_rds_app_vpc.id
+  cidr_block              = "10.0.8.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "eks-msk-rds-app-subnet-rds-2"
+  }
+}
+
+resource "aws_route_table_association" "eks_msk_rds_app_public_association_1" {
+  subnet_id      = aws_subnet.eks_msk_rds_app_subnet_public_1.id
+  route_table_id = aws_route_table.eks_msk_rds_app_public_rt.id
+}
+
+resource "aws_route_table_association" "eks_msk_rds_app_public_association_2" {
+  subnet_id      = aws_subnet.eks_msk_rds_app_subnet_public_2.id
+  route_table_id = aws_route_table.eks_msk_rds_app_public_rt.id
+}
+
+resource "aws_route_table_association" "eks_msk_rds_app_private_association_1" {
+  subnet_id      = aws_subnet.eks_msk_rds_app_subnet_private_eks_1.id
+  route_table_id = aws_route_table.eks_msk_rds_app_private_rt.id
+}
+
+resource "aws_route_table_association" "eks_msk_rds_app_private_association_2" {
+  subnet_id      = aws_subnet.eks_msk_rds_app_subnet_private_eks_2.id
+  route_table_id = aws_route_table.eks_msk_rds_app_private_rt.id
+}
+
+
+resource "aws_route_table_association" "eks_msk_rds_app_rds_association_1" {
+  subnet_id      = aws_subnet.eks_msk_rds_app_subnet_rds_1.id
+  route_table_id = aws_route_table.eks_msk_rds_app_rds_rt.id
+}
+
+resource "aws_route_table_association" "eks_msk_rds_app_rds_association_2" {
+  subnet_id      = aws_subnet.eks_msk_rds_app_subnet_rds_2.id
+  route_table_id = aws_route_table.eks_msk_rds_app_rds_rt.id
+}
+
+resource "aws_route_table_association" "eks_msk_rds_app_msk_association_1" {
+  subnet_id      = aws_subnet.eks_msk_rds_app_subnet_private_msk_1.id
+  route_table_id = aws_route_table.eks_msk_rds_app_msk_rt.id
+}
+
+resource "aws_route_table_association" "eks_msk_rds_app_msk_association_2" {
+  subnet_id      = aws_subnet.eks_msk_rds_app_subnet_private_msk_2.id
+  route_table_id = aws_route_table.eks_msk_rds_app_msk_rt.id
+}
+
+
+# Nat Gateways
+resource "aws_eip" "nat_gw" {  
+  tags = {
+    Name = "eks-msk-rds-app-ngw"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_gw.id
+  subnet_id     = aws_subnet.eks_msk_rds_app_subnet_public_1.id
+  tags = {
+    Name = "eks-msk-rds-app-nat"
+  }
+}
+
+resource "aws_route" "private_to_nat" {
+  route_table_id         = aws_route_table.eks_msk_rds_app_private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
+}
+
+resource "aws_route" "msk_to_nat" {
+  route_table_id         = aws_route_table.eks_msk_rds_app_msk_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
+}
+
+resource "aws_route" "rds_to_nat" {
+  route_table_id         = aws_route_table.eks_msk_rds_app_rds_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
+}
