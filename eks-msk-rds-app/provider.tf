71333630c8fea8a65a79af250276ebf971dc41cb
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-10-27 01:31:30 -0400

    fix (eks mks): update aws provider version to 6.12

--- eks-msk-rds-app/provider.tf
+++ eks-msk-rds-app/provider.tf
@@ -24,7 +24,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.12"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
