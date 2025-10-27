d60975b104d7bd5106180fd768adb62fa5e7dc3c
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-10-27 16:28:53 -0400

    cleanup (eks msk): remove k8 provider, cleanup create workflow

--- eks-msk-rds-app/provider.tf
+++ eks-msk-rds-app/provider.tf
@@ -1,25 +1,21 @@
 provider "aws" {
-    region = var.aws_region
-    # Provide default tags to identify all resources
-    default_tags {
-      tags = {      
-        ManagedBy = "GitAwsTerraformProjects"
-        Project   = "eks-msk-rds-app"
-      }
+  region = var.aws_region
+  # Provide default tags to identify all resources
+  default_tags {
+    tags = {
+      ManagedBy = "GitAwsTerraformProjects"
+      Project   = "eks-msk-rds-app"
     }
+  }
 }
 
 terraform {
   required_version = ">= 1.0"
-  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.12"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23"
-    }
-  }  
+  }
 }
